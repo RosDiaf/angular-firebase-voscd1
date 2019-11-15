bd07a3d2019ec74a1f8340d4e364634f4cf85fd7
RosDiaf <ros_diaf@hotmail.com>
2019-11-15 18:58:38 +0000

    1 file modified, 3 added

---
+++ src/app/PlayerForm/player-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'player-form',
+  templateUrl: './player-form.component.html',
+  styleUrls: [`./player-form.component.scss`]
+})
+export class PlayerFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './Background/background.component';
+import { PlayerFormComponent } from './PlayerForm/player-form.component';
 import { LogoComponent } from './Logo/logo.component';
 import { PostComponent } from './Post/post.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule ],
-  declarations: [ AppComponent, BackgroundComponent, LogoComponent, PostComponent ],
+  declarations: [ 
+    AppComponent,
+    BackgroundComponent,
+    LogoComponent,
+    PostComponent,
+    PlayerFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
